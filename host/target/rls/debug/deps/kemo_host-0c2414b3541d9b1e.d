d16acd6188345f7319060bff1ca2a618fd9bed72
Diego Covarrubias <dcov@pm.me>
2020-05-02 17:57:58 -0700

    Rename server to host

---
+++ host/target/rls/debug/deps/kemo_host-0c2414b3541d9b1e.d
@@ -0,0 +1,5 @@
+/home/dc/self/kemo/host/target/rls/debug/deps/kemo_host-0c2414b3541d9b1e.rmeta: src/main.rs
+
+/home/dc/self/kemo/host/target/rls/debug/deps/kemo_host-0c2414b3541d9b1e.d: src/main.rs
+
+src/main.rs:
