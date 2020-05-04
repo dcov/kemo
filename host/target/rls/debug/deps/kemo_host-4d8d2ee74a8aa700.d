80cab9a0fdaf45cb7541565da66ec18f5dc1a1df
Diego Covarrubias <dcov@pm.me>
2020-05-04 13:20:46 -0700

    Delete /target/ build dir from host

--- host/target/rls/debug/deps/kemo_host-4d8d2ee74a8aa700.d
+++
@@ -1,5 +0,0 @@
-/home/dc/self/kemo/host/target/rls/debug/deps/kemo_host-4d8d2ee74a8aa700.rmeta: src/main.rs
-
-/home/dc/self/kemo/host/target/rls/debug/deps/kemo_host-4d8d2ee74a8aa700.d: src/main.rs
-
-src/main.rs:
